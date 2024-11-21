178469d8ad4f41cb3ecdc5f97dbc76c37abfe933
rwjacobson <rwjacobson11@gmail.com>
2024-11-21 15:16:04 -0700

    creating more function

--- recipes/main.js
+++ recipes/main.js
@@ -11,25 +11,17 @@ const randomRecipe = (list) => {
 }
 
 const recipeTemplate = (recipe) => {
-    return `<div id="recipeInfo">
-            <img id="potato" src="${recipe.image}" alt="roasted-potatoes">
+    return `<img id="potato" src="${recipe.image}" alt="roasted-potatoes">
             <div id="sideInfo">
                 <div id="tags">
-                    <p>${recipe.tags[0]}</p> 
-                    <p>${recipe.tags[1]}</p>
-                    <p>${recipe.tags[2]}</p>
+                    <p>${tagsTemplate(recipe.tags)}</p>
                 </div>
                 <h2>${recipe.name}</h2>
                 <span class="rating" role="img" aria-label="Rating: 4 out of 5 stars">
-                    <span aria-hidden="true" class="icon-star">⭐</span>
-                    <span aria-hidden="true" class="icon-star">⭐</span>
-                    <span aria-hidden="true" class="icon-star">⭐</span>
-                    <span aria-hidden="true" class="icon-star-empty">⭐</span>
-                    <span aria-hidden="true" class="icon-star-empty">☆</span>
+                    ${ratingTemplate(recipe.rating)}
                 </span>
                 <p id="description">${recipe.recipeInstructions}</p>
-            </div>
-        </div>`
+            </div>`
 }
 
 function tagsTemplate(tags) {
@@ -52,14 +44,36 @@ function ratingTemplate(rating) {
 	aria-label="Rating: ${rating} out of 5 stars"
 >`
 // our ratings are always out of 5, so create a for loop from 1 to 5
-
+    for (let i = 1; i <= 5; i++) {
 		// check to see if the current index of the loop is less than our rating
 		// if so then output a filled star
-
+        if (i < rating) {
+            html += `<span aria-hidden="true" class="icon-star">⭐</span>`
+        }
 		// else output an empty star
-
+        else {
+            html += `<span aria-hidden="true" class="icon-star-empty">☆</span>`
+        }
+    }
 	// after the loop, add the closing tag to our string
 	html += `</span>`
 	// return the html string
 	return html
 }
+
+function renderRecipes(recipeList) {
+	// get the element we will output the recipes into
+    const info = document.getElementById("recipeInfo");
+	// use the recipeTemplate function to transform our recipe objects into recipe HTML strings
+    const recipe = randomRecipe(recipes);
+	// Set the HTML strings as the innerHTML of our output element.
+    info.innerHTML = recipeTemplate(recipe);
+}
+
+function init() {
+  // get a random recipe
+  const recipe = randomRecipe(recipes)
+  // render the recipe with renderRecipes.
+  renderRecipes([recipe]);
+}
+init();
